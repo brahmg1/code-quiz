26376c7f6e6516dddba187955759125571c6ad32
Brahm Gicheru <brahmgicheru@brahms-mbp.myfiosgateway.com>
2021-12-27 06:53:04 -0500

    add questions

---
+++ Develop/assets/js/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+    {q: "Commonly used data types DO Not Include:",
+    a: "alerts",
+    options: ["strings","booleans", "alerts", "numbers"]
+    },
+    {q: "The condition in an if / else statement is enclosed with __________.",
+    a: "parenthesis",
+    options: ["quotes", "curly brackets", "parenthesis", "square brackets"]
+    },
+    {q: "Arrays in JavaScript can be used to store __________.",
+    a: "all of the above",
+    options: ["numbers and strings", "other arrays", "booleans", "all of the above"]
+    },
+    {q: "String values must be enclosed within __________ when being assigned to variables.",
+    a: "quotes",
+    options: ["commas", "curly brackets", "quotes", "parenthesis"]
+    },
+    {q: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    a: "console.log",
+    options: ["JavaScript", "terminal/bash", "for loops", "console.log"]
+    },
+]
